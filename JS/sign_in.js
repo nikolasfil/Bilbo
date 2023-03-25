60992ee6d22c03601e859db10872916fedb89255
nikolasfil <nikolas13in@gmail.com>
2023-03-25 14:18:37 +0200

    updating sign ins

--- JS/sign_in.js
+++ JS/sign_in.js
@@ -1,7 +1,7 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById("sign-in-popup").style.display = "block";
   }
 
   function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+    document.getElementById("sign-in-popup").style.display = "none";
   }
