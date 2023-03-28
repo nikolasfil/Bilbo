bddd88ab07e466b750647d2523647bf7c5a1bf04
nikolasfil <nikolas13in@gmail.com>
2023-03-29 01:52:31 +0300

    search bar goes to search with enter

---
+++ JS/search_bar.js
@@ -0,0 +1,16 @@
+function clickPress(event) {
+    if (event.key == "Enter") {
+        if (event.target.value != "") {
+            // .toLocaleDateString()
+            window.location = "search.html";
+            // window.location.href = "/search/" + event.target.value;
+            event.target.value="";
+        }
+    }
+}
+
+
+let searchbar = window.document.getElementsByClassName("searchinput")[0];
+
+// searchbar.addEventListener("onkeydown", function(event) {clickPress(event);});
+searchbar.setAttribute("onkeydown", "clickPress(event);");
