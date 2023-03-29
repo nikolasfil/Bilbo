fd437509ba096fb0fe91caa9a5168d9cfe3a234a
nikolasfil <nikolas13in@gmail.com>
2023-03-29 11:27:57 +0300

    library before adding js

---
+++ JS/library_info.js
@@ -0,0 +1,21 @@
+{/* <div id="library-info-main" class="vertically-center-nm">
+
+
+    <div class="horizontally-start-nm div-paragraph-margins-55-160" id="library-info-">
+      <img src="../img/library_image_1.svg" alt="library image" style="width:282; height:282;">
+      <div class="vertically-start-nm paragraph-margins-58-79">
+        <!-- the margins are not working -->
+        <div id="library-title ">
+          <h1>Library Of Patras</h1>
+          <h3>Ypatias 4, Panepstimioupoli Patron, 265 04</h3>
+        </div>
+        <div id="library-info">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero possimus hic laboriosam perferendis, veritatis corrupti assumenda reprehenderit ducimus dignissimos quia, doloribus unde! Fugit quas minus est ex ratione dolor possimus!Lorem</p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>  */}
+
+  
