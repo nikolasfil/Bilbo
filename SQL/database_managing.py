6147909032b32feded06494be7f2c862da2765f7
nikolasfil <nikolas13in@gmail.com>
2023-04-05 01:07:20 +0300

    inserting values works ,needs some more work

--- SQL/database_managing.py
+++ SQL/database_managing.py
@@ -17,6 +17,7 @@ class Creation:
         try:
             self.conn = sq.connect(database)
             self.sqlfile = sqlfile
+            self.salt = bcrypt.gensalt()
         except Exception as e:
             print(e)
             sys.exit()
@@ -24,12 +25,75 @@ class Creation:
     def create_database(self):
         sql = open(self.sqlfile, 'r', encoding='utf-8-sig').read()
         self.conn.executescript(sql)
-        self.tables = [table_name[0] for table_name in self.conn.execute('SELECT name FROM sqlite_master WHERE type="table"').fetchall()]
+        self.table_names = [table_name[0] for table_name in self.conn.execute('SELECT name FROM sqlite_master WHERE type="table"').fetchall()]
+        # print(self.table_names)
+        self.tables = {table_name: [column[1] for column in self.conn.execute(f'PRAGMA table_info({table_name})')] for table_name in self.table_names }
+        # print([i for i in self.conn.execute("SELECT sql FROM sqlite_master WHERE tbl_name = 'table_name' AND type = 'table'")])
+        #  "SELECT sql FROM sqlite_master WHERE tbl_name = 'table_name' AND type = 'table'"
         # print(self.tables)
-        
+
+
+
+
+
+    def clearing(self,tablename:str):
+        '''clears the specific tablename from the values, but does not delete the columns'''
+        self.conn.execute(f'DELETE FROM {tablename}')
+        self.conn.commit()
+
+    def clear_all(self):
+        '''clears all tables'''
+        for table in self.table_names:
+            self.clearing(table)
 
     def fill_database(self):
-        pass
+        self.clear_all()    
+
+        # Change the binary data !!! 
+
+        # {'LIBRARY':   ['id', 'location', 'profile_picture', 'summary', 'working_hours'], 
+        #  'BOOK':      ['isbn', 'title', 'author', 'edition', 'publisher', 'release', 'genre', 'language', 'summary', 'cover_image'], 
+        #  'USER':      ['id', 'fname', 'lname', 'birthdate', 'password', 'salt', 'profile_picture'],
+        #  'Borrowing': ['book_isbn', 'copy_num', 'library_id', 'user_id', 'date_borrowing', 'date_return'], 
+        #  'Return':    ['book_isbn', 'copy_num', 'library_id', 'user_id', 'date_of_borrowing', 'date_of_return'],
+        #  'COPIES':    ['book_isbn', 'copy_num']}
+        
+        self.insert_data('LIBRARY',('a','location','profile_picture','summary','working_hours'))
+        
+        self.insert_data('BOOK',('isbn','title','author','edition','publisher','release','genre','language',
+        'summary','cover_image'))
+        
+        self.insert_data('COPIES',('book_isbn','1'))
+
+        # Users
+        # self.hashing_password(fname[i]) ,self.stringing_from_double(self.salt)
+        self.insert_data('USER',('id','fname','lname','birthdate',self.hashing_password('password') ,self.stringing_from_double(self.salt),'profile_picture'))
+        
+        self.insert_data('Borrowing',('book_isbn','copy_num','library_id','user_id','date_borrowing','date_return'))
+        
+        self.insert_data('Return',('book_isbn','copy_num','library_id','user_id','date_of_borrowing','date_of_return'))
+        
+
+    # make it clearer
+    
+    def stringify(self, string):
+        return f"'{str(string)}'"
+    
+    def stringing_from_double(self,string):
+        # return f"'{str(string)[2:-1]}'"
+        return f"{str(string)[2:-1]}"
+
+    def insert_data(self,table_name,data):
+        command = f"INSERT INTO {table_name}({','.join(self.tables[table_name])}) VALUES ({','.join(list(map(self.stringify,data)))})"
+        print(command)
+        self.conn.execute(command)
+        self.conn.commit()
+    
+
+    def hashing_password(self,password):
+        return self.stringing_from_double(bcrypt.hashpw(password.encode('utf-8'),self.salt))
+
+
 
     def main(self):
         self.create_database()
