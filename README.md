904f57296edde21fcd04580e047c03dc5b6e113d
nikolasfil <nikolas13in@gmail.com>
2023-04-17 21:16:37 +0300

    finishing the tutorial

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 
 npm install nodemon -g
+
+Start the server with 
+
+```bash
+npm start
+```
+
+And it automatically loads the changes while working on the files 
--- index.js
+++ index.js
@@ -4,14 +4,37 @@ const port = 8080;
 const expbs = require('express-handlebars');
 const path = require('path');
 
-
-
-app.engine('handlebars', expbs.engine({
+// helpers
+const hbs = expbs.create({
     defaultLayout: 'main',
     layoutsDir: path.join(__dirname, 'views/layouts'),
-    partialsDir: path.join(__dirname, 'views/partials')
-}));
-app.set('view engine', 'handlebars');
+    partialsDir: path.join(__dirname, 'views/partials'),
+    extname: '.hbs',
+
+    // create custom helpers
+    helpers: {
+        // use them as {{calculation value}}
+        calculation: function(value){
+            return value + 100;
+        },
+        list: function(value,options){
+            // return "<h2>"+options.fn({test: value})+"</h2>";
+            let out = "<ul>";
+            for(let i=0; i<value.length; i++){
+                // out = out + "<li>"+options.fn({firstName: '',lastName: ''})+"</li>";
+                out = out + "<li>"+options.fn(value[i])+"</li>";
+            }
+            return out + "</ul>";
+        }
+    }
+});
+
+
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+
 
 
 // routing 
@@ -20,6 +43,11 @@ app.get('/', (req, res) => {
         title: 'Home',
         name: 'Hello World!',
         isCompleted: true,
+        people: [
+            {firstName: 'John', lastName: 'Doe'},
+            {firstName: 'Jane', lastName: 'Doe'},
+            {firstName: 'Joe', lastName: 'Doe'},
+        ]
     });
 });
 
@@ -66,6 +94,25 @@ app.get('/each/helper', (req, res) => {
     })
 })
 
+
+app.get('/look', (req, res) => {
+    res.render('lookup',{
+        user: {
+            name: "John Doe",
+            age: 30,
+            email: "test"
+        },
+        people: [
+            'John',
+            'Jane',
+            'Joe'
+        ]
+    });
+});
+
+
+
+// final command to have the server running
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
