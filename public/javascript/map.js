e424ccbf470c2243126d529826e3f90c1777034c
nikolasfil <nikolas13in@gmail.com>
2023-04-28 17:09:29 +0300

    updated

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,36 +1,46 @@
-// // ------------------  Map ------------------
-// mapInit();
-
-// function mapInit() {
-//     map = new OpenLayers.Map("basicMap");
-//     let mapnik = new OpenLayers.Layer.OSM();
-//     let fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-//     let toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-//     // 38.2903577,21.7911003
-//     let position = new OpenLayers.LonLat(13.41, 52.52).transform(fromProjection, toProjection);
-//     // bounding box 
-//     // let position       = new OpenLayers.LonLat(38.2903577,21.7911003).transform( fromProjection, toProjection);
-//     let zoom = 5;
-
-//     map.addLayer(mapnik);
-//     map.setCenter(position, zoom);
-// }
-
-import 'style.css';
 import Map from 'ol/Map.js';
+import View from 'ol/View.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
 
-const map = new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM(),
-        }),
-    ],
-    view: new View({
-        center: [0, 0],
-        zoom: 2,
-    }),
+new Map({
+  layers: [
+    new TileLayer({source: new OSM()}),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+  target: 'map',
 });
+
+
+// import Map from 'ol/Map';
+// import OSM from 'ol/source/OSM';
+// import TileLayer from 'ol/layer/Tile';
+// import View from 'ol/View';
+
+// const map = new Map({
+//   layers: [
+//     new TileLayer({
+//       source: new OSM(),
+//     }),
+//   ],
+//   target: 'map',
+//   view: new View({
+//     center: [0, 0],
+//     zoom: 2,
+//   }),
+// });
+
+// document.getElementById('zoom-out').onclick = function () {
+//   const view = map.getView();
+//   const zoom = view.getZoom();
+//   view.setZoom(zoom - 1);
+// };
+
+// document.getElementById('zoom-in').onclick = function () {
+//   const view = map.getView();
+//   const zoom = view.getZoom();
+//   view.setZoom(zoom + 1);
+// };
