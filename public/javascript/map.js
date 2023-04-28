cac06121116108c10f2f4d6c040cdb0c3ad5cc3a
nikolasfil <nikolas13in@gmail.com>
2023-04-28 22:49:04 +0300

    spaced out the elemetns

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,7 +1,7 @@
 
 
 function mapInit(lon,lat,zoom) {
-    console.log(lon,lat,zoom);
+    // console.log(lon,lat,zoom);
     let map = new ol.Map({
         target: 'map',
         layers: [
@@ -14,4 +14,21 @@ function mapInit(lon,lat,zoom) {
             zoom: zoom
         })
     });
+
+    // const center = map.getView().getCenter();
+    // const pinnedLocation = ol.proj.transform(center, 'EPSG:3857', 'EPSG:4326');
+    // const feature = new ol.Feature(new ol.geom.Point(center));
+    // const pinLayer = new ol.layer.Vector ({
+    //   source: new ol.source.Vector ({
+    //     features: [feature]
+    //   }),
+    //   style: new ol.style.Style ({
+    //     image: new ol.style.Icon({
+    //       src: 'http://openlayers.org/en/v3.8.2/examples/data/icon.png'
+    //     })
+    //   })
+    // });
+    // map.addLayer (pinLayer);
+
+
 }
