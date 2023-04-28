e113dc8d45992a01652116dbeb31cc9b4421d00e
nikolasfil <nikolas13in@gmail.com>
2023-04-28 23:10:23 +0300

    working with a marker

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,12 +1,33 @@
 
 
-function mapInit(lon,lat,zoom) {
+// 
+function mapInit(lon, lat, zoom) {
     // console.log(lon,lat,zoom);
-    let map = new ol.Map({
+
+
+    const iconFeature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat])),
+        name: 'Somewhere near Nottingham',
+    });
+
+    const map = new ol.Map({
         target: 'map',
         layers: [
             new ol.layer.Tile({
-                source: new ol.source.OSM()
+                source: new ol.source.OSM(),
+            }),
+            new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    features: [iconFeature]
+                }),
+                style: new ol.style.Style({
+                    image: new ol.style.Icon({
+                        anchor: [0.5, 46],
+                        anchorXUnits: 'fraction',
+                        anchorYUnits: 'pixels',
+                        src: 'https://openlayers.org/en/latest/examples/data/icon.png'
+                    })
+                })
             })
         ],
         view: new ol.View({
@@ -15,20 +36,40 @@ function mapInit(lon,lat,zoom) {
         })
     });
 
+
+    // const iconFeature = new ol.Feature({
+    //     geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat])),
+    //     name: 'Null Island',
+    // });
+
+    // let map = new ol.Map({
+    //     target: 'map',
+    //     layers: [
+    //         new ol.layer.Tile({
+    //             source: new ol.source.OSM()
+    //         })
+    //     ],
+    //     view: new ol.View({
+    //         center: ol.proj.fromLonLat([lon, lat]),
+    //         zoom: zoom
+    //     })
+    // });
+
     // const center = map.getView().getCenter();
     // const pinnedLocation = ol.proj.transform(center, 'EPSG:3857', 'EPSG:4326');
     // const feature = new ol.Feature(new ol.geom.Point(center));
-    // const pinLayer = new ol.layer.Vector ({
-    //   source: new ol.source.Vector ({
-    //     features: [feature]
-    //   }),
-    //   style: new ol.style.Style ({
-    //     image: new ol.style.Icon({
-    //       src: 'http://openlayers.org/en/v3.8.2/examples/data/icon.png'
+    // const pinLayer = new ol.layer.Vector({
+    //     source: new ol.source.Vector({
+    //         features: [feature]
+    //     }),
+    //     style: new ol.style.Style({
+    //         image: new ol.style.Icon({
+    //             // src: 'http://openlayers.org/en/v3.8.2/examples/data/icon.png'
+    //             src: 'images/pin'
+    //         })
     //     })
-    //   })
     // });
-    // map.addLayer (pinLayer);
+    // map.addLayer(pinLayer);
 
 
 }
