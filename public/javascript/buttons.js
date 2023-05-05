be8f802ac7f263c95d57b0c7e78c48d2b6ec542c
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2023-05-05 15:42:23 +0300

    button.js change

--- public/javascript/buttons.js
+++ public/javascript/buttons.js
@@ -1,10 +1,12 @@
-jQuery(function($) {
+
+function resrveButton(event){
+    console.log("entered");
     $('#reservation').on('click', function() {
       var $el = $(this),
         textNode = this.lastChild;
-      $el.find('svg').toggleClass('bi bi-cart-plus bi-cart-x-fill');
+      $el.find('svg').toggleClass('bi-cart-plus bi-cart-x-fill');
       $el.toggleClass('active');
     });
-  });
+}
 
   
