afb1852e6f8bae37a4341b4ee5eb356ce94e7f97
nikolasfil <nikolas13in@gmail.com>
2023-05-08 16:00:32 +0300

    added show more to the filters

--- public/javascript/filters.js
+++ public/javascript/filters.js
@@ -54,3 +54,29 @@ function addedFilter(filterName) {
 
 // LIKE *TiTle* in books for database 
 
+function addShowMore(){
+
+    // get all the buttons that show more, and add listeners to their div , to show it or to hide it
+    // and change the button to show 
+    let divs = document.getElementsByClassName('div-show-more');
+    for (let i=0; i<divs.length; i++){
+        let button = divs[i].getElementsByClassName('btn-show-more')[0];
+        let list = divs[i].getElementsByClassName('scrollable-show-more')[0];
+        button.addEventListener('click', function(){
+            if (this.textContent == 'Show More'){
+
+                this.textContent = 'Show Less';
+                list.classList = 'd-flex flex-column justify-content-start scrollable-show-more'
+            }
+            else{
+                this.textContent = 'Show More';
+                list.classList = 'scrollable-show-more collapsed'
+            }
+        }
+        )
+    }   
+}
+
+function showMore(){
+
+}
--- routes/route_search.js
+++ routes/route_search.js
@@ -8,13 +8,21 @@ let signedIn = module.exports.signedIn;
 
 router.get('/search', (req, res) => {
     // more to be added later
+    let command
+
     let genreList = [
         { name: 'Fantasy' },
         { name: 'Science' },
         { name: 'Horror' },
         { name: 'Comedy' },
-        { name: 'Sci-fi' }
+        { name: 'Sci-fi' },
+        { name: 'Fiction' },
     ]
+
+    command = "Select distinct genre as name from BOOK where genre IS not NUll order by name"
+    let all_genreList = helpers.databaseAllCommand(command);
+
+    // remove the duplicates from the all List
 
     let availabilityList = [
         { name: 'Available' },
@@ -31,13 +39,17 @@ router.get('/search', (req, res) => {
         { name: 'Simon & Schuster' }
     ]
 
+    command = "Select distinct publisher as name from BOOK where publisher IS not NUll"
+    let all_publisherList = helpers.databaseAllCommand(command);
+
     let editionList = [
         { name: '1st' },
         { name: '2nd' },
         { name: '3rd' },
         { name: '> 3rd' }]
 
-    let command 
+    command = "Select distinct edition as name from BOOK where edition IS not NUll"
+    let all_editionList = helpers.databaseAllCommand(command);
     
     // command = `Select isbn,title,cover_image as photo from BOOK limit 6`;
     // let bookList = helpers.databaseAllCommand(command);
@@ -59,10 +71,16 @@ router.get('/search', (req, res) => {
         title: 'Search',
         style: 'search.css',
         genre: genreList,
+        all_genre: all_genreList,
+        
         availability: availabilityList,
         publisher: publisherList,
+        all_publisher: all_publisherList,
+
         library: libraryList,
         edition: editionList,
+        all_edition: all_editionList,
+
         language: languageList,
 
         signedIn: signedIn,
