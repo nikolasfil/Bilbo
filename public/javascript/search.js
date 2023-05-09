0a5a2d3921c1e2cc81533c64968aeebb8238fd95
nikolasfil <nikolas13in@gmail.com>
2023-05-10 01:01:47 +0300

    changed everything related to communication with database

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -34,6 +34,34 @@ function searchGet() {
 
 
 
+function fetchAllBooks(){
+
+    fetch("/all")
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+
+            // // get the html element which will contain the data
+            // let searchResults = document.getElementById("results");
+
+            // // clear the searchResults
+            // searchResults.innerHTML = "";
+
+            // // loop through all the data
+            // for (let i = 0; i < data.length; i++) {
+            //     // check if the input matches any of the data
+            //     // if (data[i].name.toLowerCase().includes(search.toLowerCase())) {
+            //         // if the input matches any of the data, add it to the searchResults
+            //         // searchResults.innerHTML += `{{> card photo=${data[i].photo} title=${data[i].title} description='description' copies='2' location=( encode (concat 'book_info/' ${data[i].isbn} ))}}`
+                    
+            //         searchResults.innerHTML+=`<a href="/book_info/${data[i].isbn}">${data[i].isbn}</a><br><br>`;
+            //     // }
+            // }
+        });
+
+}
 
 
 
