ca86e2adee805c42ed7f58aaf5ce71a6f8533dc2
nikolasfil <nikolas13in@gmail.com>
2023-05-21 15:34:13 +0300

    working on queries

---
+++ docs/sql/sql_queries.sql
@@ -0,0 +1,12 @@
+select * from BOOK join COPIES where isbn=book_isbn or isbn not in (select isbn from Borrowing);
+
+SELECT b.isbn, b.title
+FROM book AS b
+LEFT JOIN borrowing AS br ON b.isbn = br.book_isbn
+LEFT JOIN return AS r ON b.isbn = r.book_isbn
+WHERE br.book_isbn IS NULL OR r.book_isbn IS NOT NULL;
+
+
+SELECT *
+FROM your_table
+WHERE return_date <= date('now');
