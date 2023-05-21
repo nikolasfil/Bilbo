2d2b4c3b6e51e3774d0ced94e7b9330fada0284d
nikolasfil <nikolas13in@gmail.com>
2023-05-21 19:43:28 +0300

    really fixed titles

--- model/database_managing.py
+++ model/database_managing.py
@@ -299,7 +299,7 @@ class Data:
         for title in self.titles:
             if os.path.exists(f'{self.path}/{title}.json'):
                 continue
-            self.save_bookdata(title)
+            self.save_bookdata(title.strip())
 
     def main(self):
         self.load_books()
